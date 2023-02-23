6b666e742e1ef3ef569358cdbc30f88de62df799
Marvin Kome <marvin.kome@movinga.com>
2023-02-23 14:01:46 +0100

    finish fut champ card

--- app/playground/cards/futcard.tsx
+++ app/playground/cards/futcard.tsx
@@ -4,16 +4,15 @@ import Image from "next/image";
 const FutCard = () => {
   return (
     <div className="relative shadow bg-[#fef3ec] rounded-lg p-3 transition-transform duration-150 ease-in hover:bg-neutral-100 hover:scale-[0.97]">
-      <h3 className="font-serif text-xl font-light mb-2 font-serif-variation">Weekend League</h3>
+      <h3 className="font-serif text-xl font-light mb-4 font-serif-variation">FUT Champions</h3>
 
       <div className="flex items-center">
         <div className="mr-3">
-          <Image src={futChamps} alt="Song name" width={35} className="w-full rounded-lg" />
+          <Image src={futChamps} alt="fut champs logo" width={35} className="w-full rounded-lg" />
         </div>
-
-        <div>
-          <p className="font-semibold text-sm line-clamp-1">Rank V</p>
-          <p className="font-medium text-xs line-clamp-1">51 points</p>
+        <div className="">
+          <p className="font-medium text-md font-serif">Rank V</p>
+          <p className="font-medium text-sm text-neutral-600">11 wins</p>
         </div>
       </div>
     </div>
